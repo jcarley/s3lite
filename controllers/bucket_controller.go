81632c81674a0d744a930258ac300f4a75b680c7
jcarley <jcarley@gettyimages.com>
2016-03-23 22:21:58 -0500

    Stubs out bucket controller

--- controllers/bucket_controller.go
+++ controllers/bucket_controller.go
@@ -1,1 +1,43 @@
 package controllers
+
+import (
+	"net/http"
+)
+
+type BucketController struct {
+}
+
+// Buckets
+func (this *BucketController) CreateBucket(rw http.ResponseWriter, req *http.Request) {
+}
+
+func (this *BucketController) DeleteBucket(rw http.ResponseWriter, req *http.Request) {
+}
+
+func (this *BucketController) HeadBucket(rw http.ResponseWriter, req *http.Request) {
+}
+
+func (this *BucketController) ListBuckets(rw http.ResponseWriter, req *http.Request) {
+}
+
+// Objects
+func (this *BucketController) CopyObject(rw http.ResponseWriter, req *http.Request) {
+}
+
+func (this *BucketController) DeleteObject(rw http.ResponseWriter, req *http.Request) {
+}
+
+func (this *BucketController) DeleteObjects(rw http.ResponseWriter, req *http.Request) {
+}
+
+func (this *BucketController) GetObject(rw http.ResponseWriter, req *http.Request) {
+}
+
+func (this *BucketController) HeadObject(rw http.ResponseWriter, req *http.Request) {
+}
+
+func (this *BucketController) ListObjects(rw http.ResponseWriter, req *http.Request) {
+}
+
+func (this *BucketController) PutObject(rw http.ResponseWriter, req *http.Request) {
+}
